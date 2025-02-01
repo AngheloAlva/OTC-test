319fe46f828ac5561fb9ee6d96005c105909b943
Anghelo Alva <ance.anghelo@gmail.com>
2025-02-01 12:41:41 -0300

    Add initial database seeding, configuration, and utility functions

---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@libsql/client"
+import { drizzle } from "drizzle-orm/libsql"
+
+//! Uncomment the following line if you need to use local environment variables - Probably to seed the database
+// import { config } from "dotenv"
+// config({ path: ".env.local" })
+
+const client = createClient({
+	url: process.env.TURSO_DATABASE_URL!,
+	authToken: process.env.TURSO_AUTH_TOKEN!,
+})
+
+export const db = drizzle(client)
---
+++ src/db/seed.ts
@@ -0,0 +1,6 @@
+async function seed() {
+	console.log("Seeding database...")
+	console.log("Seed data inserted successfully")
+}
+
+seed().catch(console.error)
