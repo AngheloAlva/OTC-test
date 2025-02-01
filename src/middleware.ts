6499f1dd6fbe4acd4500940172765389d38eefa9
Anghelo Alva <ance.anghelo@gmail.com>
2025-02-01 18:01:11 -0300

    Add better-fetch integration and update authentication middleware; create initial database migration files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,17 @@
+import { betterFetch } from "@better-fetch/fetch"
 import type { auth } from "@/lib/auth"
 import { NextResponse, type NextRequest } from "next/server"
 
 type Session = typeof auth.$Infer.Session
 
 export default async function authMiddleware(request: NextRequest) {
-	const { data: session }: { data: Session } = await fetch("/api/auth/get-session", {
-		method: "POST",
-		body: JSON.stringify({
-			baseURL: request.nextUrl.origin,
-			headers: {
-				cookie: request.headers.get("cookie") || "",
-			},
-		}),
-	}).then((res) => res.json())
+	const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
+		baseURL: request.nextUrl.origin,
+		headers: {
+			//get the cookie from the request
+			cookie: request.headers.get("cookie") || "",
+		},
+	})
 
 	if (!session) {
 		return NextResponse.redirect(new URL("/sign-in", request.url))
@@ -21,5 +20,5 @@ export default async function authMiddleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/dashboard/*"],
+	matcher: "/dashboard/:path*",
 }
